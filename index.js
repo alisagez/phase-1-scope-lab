451e09b6f5a59abc402dfa183650c2a19b131a94
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-07 22:26:37 -0700

    commit tree 81484ad5c679fb933bae01ec2e5a0d5bc861a77d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+//ABOUT: A word of warning. In this lab, you will practice doing some bad things. For example, we'll ask you to declare variables in global scope, and write functions that break down. You'll also need to use var to declare all variables except when specifically instructed otherwise. We do this because, by testing the limits and exposing the underbelly of a language, you can better understand it and know what to avoid in the future.
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'nobody'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'nobody 2'
+    return leastFavoriteCustomer
+}
